935383461c4b83c315ec390af54e2499e78ad514
Andrew Zirkel <azirkel@umasd.org>
2015-11-26 16:25:39 -0500

    Initial Commit

---
+++ sidebartool/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sidebartool
+//
+//  Created by Zirkel, Andrew D. on 11/26/15.
+//  Copyright © 2015 Zirkel, Andrew D. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
